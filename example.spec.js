fde9bd5fef176c56c248cd924cdb5995f0b4cb53
hsw6281913 <hsw628@gmail.com>
2025-11-10 19:31:52 +0900

    수정버전

--- example.spec.js
+++ example.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
-test ('Playwright test demo' , async ({page}) => {
-    await page.goto('https://www.naver.com');
-    await expect(page).toHaveTitle(/naver/i);
-    await page.screenshot({path: 'example.png'});
+
+test('Playwright test demo', async ({ page }) => {
+  await page.goto('https://www.naver.com');
+  await expect(page).toHaveTitle(/naver/i);
 });
