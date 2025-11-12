7b7727234f823ad7ef335041420bb503997ccf7a
hsw6281913 <hsw628@gmail.com>
2025-11-12 18:02:16 +0900

    로그인 페이지까지

--- example.spec.js
+++ example.spec.js
@@ -12,4 +12,14 @@ test('로그인 버튼 찾기', async ({page}) => {
   await page.waitForSelector('text=로그인/회원가입', { timeout: 15000});
   const loginbtn = page.getByRole('button', {name:/로그인\/회원가입/i});
   await expect(loginbtn).toBeVisible();
+  await loginbtn.click();
+})
+
+test('로그인 페이지 노출 확인',async ({page}) => {
+  await page.goto('/');
+  await page.waitForSelector('text=로그인/회원가입', { timeout: 15000});
+  const loginbtn = page.getByRole('button', {name:/로그인\/회원가입/i});
+  await loginbtn.click();
+  await expect(page).toHaveURL (/account\.wadiz\.kr\/login/i);
+  await expect(page.getByRole('button',{ name: /이메일로 시작하기/i})).toBeVisible();
 })
