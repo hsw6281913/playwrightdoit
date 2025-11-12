5ebe22b155bcdce1a6c529aa68ddf59e1d614df0
hsw6281913 <hsw628@gmail.com>
2025-11-12 16:35:19 +0900

    11월 12일

--- example.spec.js
+++ example.spec.js
@@ -1,6 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('Playwright test demo', async ({ page }) => {
-  await page.goto('https://www.naver.com');
-  await expect(page).toHaveTitle(/naver/i);
+test('wadiz main connection', async ({ page }) => {
+  await page.goto('https://www.wadiz.kr');
+  await expect(page).toHaveURL(/wadiz/i);
 });
+
+
+test('로그인 버튼 찾기', async ({page}) => {
+
+}
+  await page.goto('/');
+  const cosent = page.getbyRole('button', {name:/로그인/i});
